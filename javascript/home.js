3e0946c29fe2f04cbfce7369f23a057ac59483e8
Frederick <westbrookfrederick@gmail.com>
2019-02-25 01:48:55 -0600

    Initial commit

--- javascript/home.js
+++ javascript/home.js
@@ -1,22 +1,22 @@
-//Event Slide
-let slideIndex = 0;
-showSlides();
+// //Event Slide
+// let slideIndex = 0;
+// showSlides();
 
-function showSlides() {
-  let i;
-  const slides = document.getElementsByClassName("event");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.opacity = "0";
-    slides[i].style.height = "0%";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  slides[slideIndex - 1].style.opacity = 1;
-  slides[slideIndex - 1].style.height = "70%";
-  setTimeout(showSlides, 4000);
-}
+// function showSlides() {
+//   let i;
+//   const slides = document.getElementsByClassName("event");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.opacity = "0";
+//     slides[i].style.height = "0%";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {
+//     slideIndex = 1;
+//   }
+//   slides[slideIndex - 1].style.opacity = 1;
+//   slides[slideIndex - 1].style.height = "70%";
+//   setTimeout(showSlides, 4000);
+// }
 //Testimonials Slide
 let slideBase = 0;
 showTest();
